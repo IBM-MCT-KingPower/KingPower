588f3f682bff561b1468dff00a12a31a0a011024
dayhunter <patis.pn@gmail.com>
2015-10-25 12:48:49 +0700

    Patis FlightViewController
    Patis FlightViewController

--- KingPowerUsingCoreData/View/FlightViewController.swift
+++ KingPowerUsingCoreData/View/FlightViewController.swift
@@ -15,8 +15,97 @@ class FlightViewController: UIViewController {
     @IBOutlet weak var vMain: UIView!
     @IBOutlet weak var btnOk: UIButton!
     @IBOutlet weak var lblFlight: UILabel!
+    @IBOutlet weak var lblTime: UILabel!
     
+    var gv = GlobalVariable()
+    var flight : FlightInfoModel? = FlightInfoModel()
     
+    override func viewWillAppear(animated: Bool) {
+        let prefs:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        let custId: Int32 = Int32(prefs.integerForKey(gv.getConfigValue("currentCustomerId") as! String))
+        let flightArray = FlightInfoController().getFlightByCustomerId(custId)
+        
+        var hasMinFlight = Bool()
+        var minFlightDate = NSDate()
+        var message:String = "YOU DID NOT HAVE FLIGHT IN 2 DAYS"
+        var messageTime:String = ""
+        
+        for i in flightArray! {
+            let nowDate = NSDate()
+            let dateFormatter = NSDateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+            let customDate = dateFormatter.dateFromString(i.flii_flight_date)
+            /*
+            let elapsedTime = customDate!.timeIntervalSinceDate(nowDate)
+            print("patis elapsedTime " +  String(elapsedTime));
+            
+            if elapsedTime < 172800 && elapsedTime > 0 { //172800 = 2 day
+            }
+            */
+            let dayHourMinuteSecond: NSCalendarUnit = [.Day,
+                .Hour,
+                .Minute,
+                .Second]
+            let difference = NSCalendar.currentCalendar().components(
+                dayHourMinuteSecond,
+                fromDate: nowDate,
+                toDate: customDate!,
+                options: [])
+            
+            if difference.day > 2 || difference.day < 0 || difference.hour < 0 || difference.minute < 0 || difference.second < 0 {
+                hasMinFlight = false
+            }
+            else{
+                var recordNew:Bool = false
+                if hasMinFlight == false {
+                    hasMinFlight = true
+                    recordNew = true
+                    minFlightDate = customDate!
+                }
+                else{
+                    let elapsedTime = customDate!.timeIntervalSinceDate(minFlightDate)
+                    
+                    if elapsedTime < 0 {
+                        minFlightDate = customDate!
+                        recordNew = true
+                    }
+                }
+                if recordNew == true{
+                    message = "YOUR NEXT FLIGHT IS " + i.flii_flight_no + ". BOARDING GATE WILL CLOSE IN"
+                    messageTime = ""
+                    if difference.day > 0 {
+                        messageTime += String(difference.day)
+                        if difference.day > 1 {
+                            messageTime += " Days "
+                        }
+                        else{
+                            messageTime += " Day "
+                        }
+                    }
+                    if difference.hour > 0 {
+                        messageTime += String(difference.hour)
+                        if difference.hour > 1 {
+                            messageTime += " Hours "
+                        }
+                        else{
+                            messageTime += " Hour "
+                        }
+                    }
+                    if difference.minute > 0{
+                        messageTime += String(difference.minute)
+                        if difference.minute > 1 {
+                            messageTime += " Minutes "
+                        }
+                        else{
+                            messageTime += " Minute "
+                        }
+                    }
+                }
+            }
+        }
+        lblFlight.text = message
+        lblTime.text = messageTime
+    }
     override func viewDidAppear(animated: Bool) {
         //self.vMain.frame = CGRectMake(0, 80, 1024, 500)
         //self.vContainer.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.6)
@@ -59,13 +148,13 @@ class FlightViewController: UIViewController {
         });
     }
     
-    
     @IBAction func okTapped(sender: AnyObject) {
         removeAnimate()
     }
     
     func removeAnimate()
     {
+        
         UIView.animateWithDuration(0.5, animations: {
             self.vMain.alpha = 0.0
             self.vMain.center.y = -500
